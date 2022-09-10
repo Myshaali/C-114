bb277255a8988cafda87e5cce1f1822c0471a580
Myshaali <87828139+Myshaali@users.noreply.github.com>
2022-09-10 15:39:15 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
+righthandX=0;
+righthandY=0;
+
 function preload(){
+    butterfly = loadImage('https://i.postimg.cc/cJ0CNWhB/Butterfly-PNG-3.png')
 }
 
-function setup(){
-    canvas = createCanvas(300,300);
+function setup()
+{
+    canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
+    video.size(300, 300);
     video.hide();
 
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPose);
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video,0,0,300,300);
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
 }
 
-function take_snapshot(){
-    save("filter.png");
+function draw() 
+{
+    image(video, 0, 0, 300, 300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    ///circle(righthandX, righthandY, 20);
+    image(butterfly, righthandX, righthandY - 40, 40, 40);
 }
 
-function modelLoaded(){
-    console.log("poseNet Is Initialized");
+function take_snapshot()
+{
+    save('Filterimage.png');
 }
 
-function gotPose(results){
-    if(results.length > 0){
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
         console.log(results);
-        console.log("nose x: "+results[0].pose.nose.x);
-        console.log("nose y: "+results[0].pose.nose.y);
-    };
+        righthandX = results[0].pose.rightWrist.x;
+        righthandY = results[0].pose.rightWrist.y;
+        console.log("nose x = " +results[0].pose.rightWrist.x);
+        console.log("nose y = " +results[0].pose.rightWrist.y);
+    }
 }
